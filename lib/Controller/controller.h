8e9064e54e2c47f35507e98954e2004c0dbd513b
David Salazar <davidfsbras@gmail.com>
2025-03-11 18:32:38 +0000

    Creation of Controller library

---
+++ lib/Controller/controller.h
@@ -0,0 +1,15 @@
+class Controller {
+
+private:
+    float settling_time, time_const;
+
+
+public:
+    Controller(float settling_time, float time_const, bool P, bool I, bool D);
+
+    
+
+
+
+
+};
