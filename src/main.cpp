4b500c9cfdd797986450b1fe4bc8d7971d033b05
David Salazar <davidfsbras@gmail.com>
2025-03-18 11:34:53 +0000

    Added a MPU6050 Accelerometer to calibrate the servo motor's angle

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,33 @@
 #include <Arduino.h>
+#include <math.h>
 #include "controller.h"
 #include <Adafruit_VL53L0X.h>
 #include <WString.h>
+#include <ServoHiwonder.hpp>
+#include <SoftwareSerial.h>
+#include <ServoHiwonderClass.hpp>
+#include <MPU6050.h>
+#include <Wire.h>
 
 #define ANGLE_MAX 30
 #define ANGLE_MIN -30
 
+#define ROLL_OFFSET -3.3
+#define PITCH_OFFSET -2.0
+
+#define MAIN 0
+#define TEST 1
+
 Adafruit_VL53L0X lox = Adafruit_VL53L0X();
 float distance[] = {0.0, 0.0, 0.0, 0.0};
+
+SoftwareSerial serial_motors(2, 3);  // RX, TX
+ServoController servoCont(serial_motors);
+
+MPU6050 mpu;
+const int MPU = 0x68;
+int16_t ax, ay, az, gx, gy, gz;
+float accelX, accelY, accelZ, roll, pitch;
 
 int dist_index = 0;
 
@@ -26,16 +46,17 @@ float Td = 0.3701;
 float distance_INPUT;
 float servo_angle_OUTPUT;
 
-float distance_REF = 10.0;
+float distance_REF = 18.0;
 float distance_ERROR = 0.0;
 
-
 Controller controller("servo angle controller", true, false, true, ANGLE_MAX, ANGLE_MIN, control_sampling_time);
+
+#if MAIN
 
 void setup() {
   
   lox.begin();
-
+  Serial.begin(115200);
   controller.setProportional(Kc);
   controller.setDerivative(Td);
 
@@ -68,16 +89,53 @@ void loop() {
 
     distance_INPUT = takeDistanceAverage();
     distance_ERROR = distance_REF - distance_INPUT;
-    servo_angle_OUTPUT = controller.computeOutput(distance_ERROR);    
+    servo_angle_OUTPUT = deg2rad(controller.computeOutput(distance_ERROR));    
 
     //write servo_angle_OUTPUT on the servo motor (...)
+    servoCont.moveWithTime(1, servo_angle_OUTPUT, 60);
 
     last_control_sample = now;
   }
-  
+}
+
+#endif
+
+#if TEST
+void setup() {
+  lox.begin();
+  Serial.begin(115200);
+  mpu.initialize();
+  Wire.begin();
+}
+
+void readIMU(){
+  mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+
+  // Convert raw accelerometer data to g-force
+  accelX = ax / 16384.0;  // MPU6050 full-scale range ±2g
+  accelY = ay / 16384.0;
+  accelZ = az / 16384.0;
+
+  // Calculate roll and pitch angles (in degrees)
+  roll  = ROLL_OFFSET + atan2(accelY, accelZ) * 180.0 / PI;
+  pitch = PITCH_OFFSET + atan2(-accelX, sqrt(accelY * accelY + accelZ * accelZ)) * 180.0 / PI;
+
+  if (roll > 180.0){roll = roll - 360.0;}
+
+  if (abs(roll) < 2.0){roll = 0.0;}
+  if (abs(pitch) < 2.0){pitch = 0.0;}
 
 }
 
+void loop() {
+  
+  readIMU();
+  Serial.println("Roll: " + String(roll) + " Pitch: " + String(pitch));
+  delay(500); // Wait before scanning again 
+
+}
+
+#endif
 
 float takeDistanceAverage(){
   float dist_sum = 0.0;
@@ -87,5 +145,9 @@ float takeDistanceAverage(){
   }
 
   return dist_sum/4.0;
+}
+
+float deg2rad(float deg){
+  return deg * PI / 180.0;
 }
 
